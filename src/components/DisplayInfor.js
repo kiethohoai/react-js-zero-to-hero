1b402e89ada99bd42081819874640ca0c923b021
kiethohoai <kietho.060193@gmail.com>
2024-04-24 03:37:53 +0700

    #24. Outputting List - DIpaly ArrayObject with React

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,14 +2,27 @@ import React from "react";
 
 class DispalyInfor extends React.Component {
    render() {
-      console.log(this.props);
       //   destructuring props
-      let { name, age } = this.props; //object
+      const { listUsers } = this.props; // const listUsers = this.props.listUsers
       return (
          <div>
-            DISPALY INFORMATION
+            {listUsers.map(user => {
+               return (
+                  <div key={user.id}>
+                     <div>My name's {user.name}</div>
+                     <div>I'm years {user.age} old!</div>
+                  </div>
+               );
+            })}
+
+            {/* <div>My name's {name}</div>
+            <div>I'm {age} years old!</div>
+            <hr />
             <div>My name's {name}</div>
             <div>I'm {age} years old!</div>
+            <hr />
+            <div>My name's {name}</div>
+            <div>I'm {age} years old!</div> */}
             {/* <div>My name's {this.props.name}</div>
             <div>I'm {this.props.age} years old!</div> */}
          </div>
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,19 @@ import React from "react";
 import UserInfor from "./UserInfor";
 import DispalyInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
+   state = {
+      listUsers: [
+         { id: 1, name: "Ho Hoai Kiet", age: 30 },
+         { id: 2, name: "Ho Hoai Tung", age: 28 },
+         { id: 3, name: "Ho Hoai San", age: 26 },
+      ],
+   };
    //JSX
    render() {
-      const myInfor = ["a", "b", "c"];
       return (
          <div>
             <UserInfor />
-            <DispalyInfor name="HO HOAI KIET" age={30} />
-            <DispalyInfor name="HO HOAI TUNG" age={28} myInfor={myInfor} />
+            <DispalyInfor listUsers={this.state.listUsers} />
          </div>
       );
    }
