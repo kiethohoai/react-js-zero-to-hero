2a63421124bd0dac66c2f6e96218f190de4adf8c
kiethohoai <kietho.060193@gmail.com>
2024-04-23 16:39:05 +0700

    #19 React DOM Events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,12 +9,26 @@ class MyComponent extends React.Component {
       address: "Hue",
    };
 
+   //Events Handle
+   handleClick(e) {
+      console.log(e.target);
+      // console.log("My name is ", this.state.name);
+   }
+
+   handleOnMoveOver(e) {
+      console.log(e.target);
+   }
+
    //JSX
    render() {
       return (
          <div>
             My English name's {this.state.name} I am {this.state.age}. and I
             live in {this.state.address} city!
+            <br />
+            <button onMouseOver={this.handleOnMoveOver}>Hover Me!</button>
+            <br />
+            <button onClick={this.handleClick}>Click Me!</button>
          </div>
       );
    }
