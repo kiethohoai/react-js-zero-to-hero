95ec5c38e58fc534d30af9fa92d85c7b79d84cd3
kiethohoai <kietho.060193@gmail.com>
2024-04-26 15:12:30 +0700

    Design Header with Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,39 +2,16 @@ import logo from "./logo.svg";
 import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
 import React from "react";
+import Header from "./components/Header/Header";
 
-//Class Component
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello ReactJS
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app-container">
+      {/* Hello World */}
+      <Header />
+    </div>
+  );
+};
 
 export default App;
-
-///////////////////////////// BACKUP APP AROW FUNC /////////////////////////////////////
-//Function Component
-// const App = () => {
-//    const count = useSelector(state => state.counter.count);
-//    const dispatch = useDispatch();
-//    return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Hi, this is my first react project!
-//           </p>
-//           <div>Count = {count}</div>
-//           <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//           <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//         </header>
-//       </div>
-//    );
-// };
---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Your Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Users</Nav.Link>
+            <Nav.Link href="#link">Admin</Nav.Link>
+          </Nav>
+
+          <Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
