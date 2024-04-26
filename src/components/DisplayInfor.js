29b146d9b92da4d9ec2439111a80dafd5be4502b
kiethohoai <kietho.060193@gmail.com>
2024-04-26 08:08:02 +0700

    #38 useEffect Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
 
@@ -40,6 +40,14 @@ const DispalyInfor = (props) => {
     setShowHideListUsers(!isShowHideListUsers);
   };
 
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("You delete all the users!");
+    }
+    console.log("Call me useEffect()");
+  }, [listUsers]);
+
+  console.log("Call me render()");
   return (
     <div className="display-infor-container">
       <div>
