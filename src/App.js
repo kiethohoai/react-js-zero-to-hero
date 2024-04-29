2851d1e7efdff7a5c625a8ca046c44729a26302d
kiethohoai <kietho.060193@gmail.com>
2024-04-29 16:57:03 +0700

    #46 Setup Bootstrap 5 & React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import React from "react";
-import Header from "./components/Header/Header";
 
 const App = () => {
-  return (
-    <div className="app-container">
-      {/* Hello World */}
-      <Header />
-    </div>
-  );
+  return <div className="app-container">Hello World</div>;
 };
 
 export default App;
