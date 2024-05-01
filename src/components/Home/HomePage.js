9abf47359368316fc808344fc49b31d8f6e006ad
kiethohoai <kietho.060193@gmail.com>
2024-05-01 16:35:01 +0700

    +52 Design Homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,23 @@
+import videoHomePage from "../../assets/video-homepage.webm";
 const HomePage = (props) => {
-  return <div className="homepage-container">Home Page Component</div>;
+  return (
+    <div className="homepage-container">
+      <div className="home-media">
+        <div className="home-content">
+          <h1 className="home-title">Make forms worth filling out</h1>
+          <p className="home-desc">
+            Get more data—like signups, feedback, and anything else—with forms
+            designed to be refreshingly different.
+          </p>
+        </div>
+        <div className="home-video">
+          <video autoPlay muted loop width="700" height="500">
+            <source src={videoHomePage} type="video/webm" />
+          </video>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
