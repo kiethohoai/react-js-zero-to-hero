890250b424fef885453c6311fa6f5cbf9f9600c2
kiethohoai <kietho.060193@gmail.com>
2024-05-04 08:52:41 +0700

    +46 Checkout Main to Practice Branch

--- src/App.js
+++ src/App.js
@@ -18,23 +18,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-///////////////////////////// BACKUP APP AROW FUNC /////////////////////////////////////
-//Function Component
-// const App = () => {
-//    const count = useSelector(state => state.counter.count);
-//    const dispatch = useDispatch();
-//    return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Hi, this is my first react project!
-//           </p>
-//           <div>Count = {count}</div>
-//           <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//           <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//         </header>
-//       </div>
-//    );
-// };
--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,36 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
 
-// class DispalyInfor extends React.Component {
-//   render() {
-//     const { listUsers } = this.props;
-//     return (
-//       <div className="display-infor-container">
-//         {true && (
-//           <div>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-//                   <hr />
-//                   <div>My name is {user.name}</div>
-//                   <div>And I am {user.age} years old.</div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
 const DispalyInfor = (props) => {
   const { listUsers } = props;
   const [isShowHideListUsers, setShowHideListUsers] = useState(true);
