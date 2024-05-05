a5b0fb58cf84dc22a5a8cc6b949ea5913635be8e
kiethohoai <kietho.060193@gmail.com>
2024-05-05 08:23:41 +0700

    +52 Design Homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,22 @@
+import video from "../../assets/video-homepage.webm";
+import "../Home/HomePage.scss";
+
 const HomePage = () => {
   return (
     <div className="homepage-container">
-      <h1>Home Page Component</h1>
+      <div className="home-content">
+        <h1>Make forms worth filling out</h1>
+        <p>
+          Get more data—like signups, feedback, and anything else—with forms
+          designed to be refreshingly different.
+        </p>
+        <button className="btn btn-dark">Get started—it's free</button>
+      </div>
+      <div className="home-media">
+        <video autoPlay loop muted>
+          <source src={video} type="video/webm" />
+        </video>
+      </div>
     </div>
   );
 };
