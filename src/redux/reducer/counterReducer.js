5ec92d6a58d320f05399af0d10262a744670624b
kiethohoai <kietho.060193@gmail.com>
2024-05-13 14:05:56 +0700

    +77

--- src/redux/reducer/counterReducer.js
+++ src/redux/reducer/counterReducer.js
@@ -1,22 +1,27 @@
+import { INCREMENT, DECREMENT } from "../action/counterAction";
 
-import { INCREMENT, DECREMENT } from '../action/counterAction';
 const INITIAL_STATE = {
-    count: 0,
-    name: 'Eric'
+  count: 0,
+  name: "Eric",
 };
-const countReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state, count: state.count + 1,
-            };
 
-        case DECREMENT:
-            return {
-                ...state, count: state.count - 1,
-            };
-        default: return state;
-    }
+const countReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default countReducer;
