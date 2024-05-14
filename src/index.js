44cb70490de163a6c152a611003d6ff87d6b3fe2
kiethohoai <kietho.060193@gmail.com>
2024-05-14 15:40:58 +0700

    +80 Redux Persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import Layout from "./Layout";
 import { BrowserRouter } from "react-router-dom";
 import "nprogress/nprogress.css";
-
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
 
