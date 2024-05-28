9fb99b8ccae9b171d9924eeb7e08a5676b0439c9
kiethohoai <kietho.060193@gmail.com>
2024-05-28 16:30:39 +0700

    +83 Process Data Ver1.0

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
   let params = useParams();
@@ -13,6 +14,45 @@ const DetailQuiz = (props) => {
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
     console.log("🚀CHECK + file: DetailQuiz.js:15 + res:", res);
+
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+
+      //loash group by
+      let data = _.chain(raw)
+        // Group the elements of Array based on `id` property
+        .groupBy("id")
+        // `key` is group's name (id), `value` is the array of objects
+        .map((value, key) => {
+          console.log("🚀CHECK + file: DetailQuiz.js:27 + value:", value);
+          console.log("🚀CHECK + file: DetailQuiz.js:27 + key:", key);
+
+          let answers = [];
+          let questionDescription = null;
+          let image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+            console.log(
+              "🚀CHECK + file: DetailQuiz.js:30 + item.answers",
+              item.answers,
+            );
+          });
+
+          return {
+            questionId: key,
+            answers: answers,
+            questionDescription: questionDescription,
+            image: image,
+          };
+        })
+        .value();
+
+      console.log("🚀CHECK + file: DetailQuiz.js:23 + data:", data);
+    }
   };
 
   return (
