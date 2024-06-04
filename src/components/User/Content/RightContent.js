672da3cb3adedf54f17c9d130b34d5f2e7a6f4be
kiethohoai <kietho.060193@gmail.com>
2024-06-04 18:21:40 +0700

    105 Select Question RightContent Ver2

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,26 +1,66 @@
+import { useEffect, useState } from "react";
 import CownDown from "./CountDown";
 import "./RightContentFix.scss";
 
 const RightContent = (props) => {
   const { dataQuiz, handleFinishQuiz, indexQ, setIndexQ } = props;
-  console.log("🚀CHECK  file: RightContent.js:6  dataQuiz =", dataQuiz);
+  console.log("🚀CHECK  file: RightContent.js:7  dataQuiz =", dataQuiz);
 
   // getClassQuestion
   const getClassQuestion = (index, question) => {
-    // console.log("🚀CHECK  file: RightContent.js:10  index, question =", index, question);
     // check answered
     if (question && question.answers.length > 0) {
       let isAnswered = question.answers.find((a) => a.isSelected === true);
+      // let isClicked =
 
       if (isAnswered) {
         return "question selected";
       }
     }
+
     return "question";
   };
 
-  const handleClickQuestion = (index, item) => {
+  // handleClickQuestion
+  const handleClickQuestion = (index, curQuestion, e) => {
+    // Index Question
     setIndexQ(index);
+
+    console.log("🚀CHECK  file: RightContent.js:26  e.target =", e.target);
+    console.log("🚀CHECK  file: RightContent.js:26  e.target.classList =", e.target.classList);
+    console.log("🚀CHECK  file: RightContent.js:26  index, curQuestion =", index, curQuestion);
+
+    // 1 xoa class clicked truoc do
+
+    const temp2 = document.querySelector(".question.selected.clicked");
+    console.log("🚀CHECK  file: RightContent.js:32  temp2 =", temp2);
+    if (temp2 !== null) {
+      temp2.classList = "question selected";
+    }
+
+    const temp1 = document.querySelector(".question.clicked");
+    console.log("🚀CHECK  file: RightContent.js:32  temp1 =", temp1);
+    if (temp1 !== null) {
+      temp1.classList = "question";
+    }
+
+    let temp3 = e.target.classList;
+
+    console.log("🚀CHECK  file: RightContent.js:47  temp3 =", temp3);
+    console.log("🚀CHECK  file: RightContent.js:49  temp3.length =", temp3.length);
+
+    console.log("🚀CHECK  file: RightContent.js:47  temp3.value =", temp3.value);
+
+    if (temp3.value === "question") {
+      e.target.classList = "question clicked";
+    }
+
+    if (temp3.value === "question selected") {
+      e.target.classList = "question selected clicked";
+    }
+
+    // e.target.classList = "question clicked";
+    // e.target.classList = "question selected clicked";
   };
 
   return (
@@ -37,7 +77,7 @@ const RightContent = (props) => {
               <div
                 key={`qabc-${index}`}
                 className={getClassQuestion(index, item)}
-                onClick={() => handleClickQuestion(index, item)}
+                onClick={(e) => handleClickQuestion(index, item, e)}
               >
                 {index + 1}
               </div>
